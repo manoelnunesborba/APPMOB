fb50a996a77f1b643ca9f0cffcc680a07db14387
SolaZn <azakani91@gmail.com>
2022-02-18 11:59:47 +0100

    test d'ajout d'un bouton

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>WSHHH LEKIPPPPPP</Text>
       <StatusBar style="auto" />
+      <Button
+        title="Appuyez ici pour continuer"
+        onPress={() => Alert.alert('HAHA GOT YOU')}
+      />
     </View>
   );
 }
