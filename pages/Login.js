11d60723eb8e12de36bf8f6f16f865d6bc2ea0ce
DVicheva <detcha.vicheva@gmail.com>
2022-02-25 23:07:59 +0100

    klm

--- pages/Login.js
+++ pages/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import FooterMenu from './../component/FooterMenu.js'
 
 class Login extends React.Component{
     render(){
