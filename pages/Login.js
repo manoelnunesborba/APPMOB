f11aa565ff56262c010fe129e6a5a41d6d5c32f9
maketheswitch <azakani91@gmail.com>
2022-02-25 12:32:05 +0100

    ajout firebase2

--- pages/Login.js
+++ pages/Login.js
@@ -1,5 +1,20 @@
 import React from 'react';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD2XKLHd0H2O0l2Ae_qXjca7KNkn3U_IuI",
+    authDomain: "topik-3ede8.firebaseapp.com",
+    projectId: "topik-3ede8",
+    storageBucket: "topik-3ede8.appspot.com",
+    messagingSenderId: "774648378964",
+    appId: "1:774648378964:web:e92ddeb98eb174bc15dfc1",
+    measurementId: "G-GNRSP1HQ59"
+  };
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 class Login extends React.Component{
     render(){
