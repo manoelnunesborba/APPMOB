e12fc2c8d36d89d8a83f6f2de9912e91d16dc638
Manoelnb <76746287+manoelnunesborba@users.noreply.github.com>
2022-02-28 15:00:23 +0100

    Update UserDetail.js

--- pages/UserDetail.js
+++ pages/UserDetail.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 class UserDetail extends React.Component{
     render(){
         return (
-            <Text>feeduser</Text>
+            <Text>User</Text>
         )
     }
 }
